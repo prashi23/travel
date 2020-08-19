e31dd93e1f8b2b267773d18f549420d781940f72
prashi23 <prashidoval23@gmail.com>
2020-08-19 10:12:21 +0530

    mongoose model setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const middlewares = require('./middlewares');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, 
+});
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
 }
 ));
 
